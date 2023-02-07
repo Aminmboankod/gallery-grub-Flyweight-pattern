961a8d529968f6469e24478ac0819023d58c7349
Amin Mustafa Boankod <amin.m.boankod@gmail.com>
2023-02-07 12:04:19 +0100

    feat(RetailPrice): Add RetailPrice feature

--- src/main/java/edu/craptocraft/items/RetailPrice.java
+++ src/main/java/edu/craptocraft/items/RetailPrice.java
@@ -1,11 +1,25 @@
 package edu.craptocraft.items;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RetailPrice {
 
+    static final Map<String, Double> prices = new HashMap<String, Double>();
+
+    public RetailPrice() {}
+
+
+
     public static void init_prices() {
-    }
+        prices.put("cheese", 0.25d);
+        prices.put("sauce", 0.50d);
+        prices.put("medium", 0.25);
+        prices.put("large", 0.50d);
+        }
 
     public static void display() {
+        prices.forEach((key, value) -> System.out.println(key+ "="+value+"$"));
     }
     
 }
--- src/test/java/edu/craptocraft/items/RetailPriceTest.java
+++ src/test/java/edu/craptocraft/items/RetailPriceTest.java
@@ -1,11 +1,36 @@
 package edu.craptocraft.items;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 import org.junit.Test;
 
 public class RetailPriceTest {
+    
+
     @Test
-    public void testConstructorRetailPrice() {
-        
+    public void testInitPrices() {
         RetailPrice.init_prices();
+        assertEquals(0.25d, RetailPrice.prices.get("cheese"), 0.001);
+        assertEquals(0.50d, RetailPrice.prices.get("sauce"), 0.001);
+        assertEquals(0.25d, RetailPrice.prices.get("medium"), 0.001);
+        assertEquals(0.50d, RetailPrice.prices.get("large"), 0.001);
+    }   
+    @Test
+    public void testAddPrice() {
+        RetailPrice.init_prices();
+        RetailPrice.prices.put("topping", 0.75d);
+        assertEquals(0.75d, RetailPrice.prices.get("topping"), 0.001);
     }
+
+    @Test
+    public void testRemovePrice() {
+        RetailPrice.init_prices();
+        RetailPrice.prices.remove("cheese");
+        assertFalse(RetailPrice.prices.containsKey("cheese"));
 }
+
+
+}
+
+
